2e72a718b22e2068407345fdfee33c97bfe6d460
AJ Wagner <ajwagner@northwesternmutual.com>
2017-05-18 11:17:31 -0500

    starting point

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,11 @@
+var api = require('./../controller/api.js')
+
 module.exports = function(app){
 	app.get('/', function(req, res){
-		res.render('index')
+		//res.render('index')
+
+		api.index(req, res);
+		//app.get('/', api.index);
 	})
 
 
